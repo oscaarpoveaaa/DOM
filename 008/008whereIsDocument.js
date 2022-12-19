cf41035944a79a495acc9cc3fdc6e70c65d4a451
Oscar Povea <oscaarpoveaa@gmail.com>
2022-12-19 14:08:34 +0100

    sig

--- 008/008whereIsDocument.js
+++ 008/008whereIsDocument.js
@@ -6,9 +6,13 @@
 "use strict";
 
 //¿Dónde está el "document" en la jerarquía?
+/* Esta por debajo de Windows pero por encima de todos los demás*/
 
 //¿A qué clase pertenece el document?
+/* Pertenece a HTMLDocument  */
 
 //¿Cuál es su lugar en la jerarquía DOM?
+/*  */
 
 //¿Hereda de Node o Element, o tal vez HTMLElement?
+/*  */
